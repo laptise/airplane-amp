69a1077be4aab1ed9b3f727db94a29f8ba08a0e7
Yoonsoo Kim <laptise@live.jp>
2023-01-22 00:58:03 +0900

    build structure

---
+++ pages/main/index.tsx
@@ -0,0 +1,12 @@
+import DefaultLayout from "@/components/layouts/top";
+import { FlexCol } from "@/components/sc/common";
+
+const MainTop = () => {
+  return (
+    <DefaultLayout>
+      <FlexCol>ss</FlexCol>
+    </DefaultLayout>
+  );
+};
+
+export default MainTop;

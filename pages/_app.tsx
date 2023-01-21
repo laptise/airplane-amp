d32d7e6e72d1dced245baa3728af32f1a7b8bbec
Yoonsoo Kim <laptise@live.jp>
2023-01-22 02:17:08 +0900

    fix

---
+++ pages/_app.tsx
@@ -0,0 +1,29 @@
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { purple } from "@mui/material/colors";
+import "../styles/globals.css";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: "#f44336",
+    },
+  },
+});
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <title />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
+);
+
+export default App;
--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,10 +1,13 @@
 import DefaultLayout from "@/components/layouts/top";
 import { FlexCol } from "@/components/sc/common";
+import { Button, TextField } from "@mui/material";
 
 const MainTop = () => {
   return (
     <DefaultLayout>
       <FlexCol>ss</FlexCol>
+      <TextField size="medium" />
+      <Button variant="contained">aa</Button>
     </DefaultLayout>
   );
 };
